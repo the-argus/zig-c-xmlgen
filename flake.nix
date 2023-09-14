1e20b33c69e2784d0977f326b43a451d95fd0f37
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-09-14 15:31:41 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Zig-C-XMLGen environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    supportedSystems = let
+      inherit (flake-utils.lib) system;
+    in [
+      system.aarch64-linux
+      system.x86_64-linux
+    ];
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          zig_0_11
+        ];
+      };
+    });
+}
