9d8ab4430623635ad1df703d34e9709443768b12
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-09-14 17:24:07 -0400

    init build system with libxml2

---
+++ build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const libxml = b.dependency("libxml", .{ .target = target, .optimize = optimize });
+
+    const main_tests = b.addTest(.{
+        .name = "test_exe",
+        .root_source_file = .{ .path = "src/test_convert.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
+
+    main_tests.linkLibrary(libxml.artifact("xml2"));
+
+    const tests_run = b.addRunArtifact(main_tests);
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&tests_run.step);
+}
